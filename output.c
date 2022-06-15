ed31e6fa802fec1f9fd93bb8777913b09729b76a
bartosz <bartoszkruczek@gmail.com>
2022-06-15 02:34:03 +0200

    cleaning

---
+++ output.c
@@ -0,0 +1,26 @@
+#include<String/def>
+#include<Integer>
+using namespace std;
+int pryw;
+int x = ( ( 2 * 7 ) / ( 11 * 28 ) ) - 2 + 4 + 5 - 2 ;
+int y = -2.2 ;
+char test1[];
+char test2[] = "testowy wyraz";
+int fun(int y,int z){
+for(int i = 0 ; i<10; i++){
+x++;
+}
+x--;
+x+=2;
+return x;
+}
+float fun2(int x){
+if(x==2){
+x=2 ;
+for(int i = 0 ; i<10; i++){
+x++;
+}
+}
+return fun(a,b);
+}
+
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,22 @@ import visitor.JavaGrLexer;
 import visitor.JavaGrParser;
 import visitor.own.JavaVisitor;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+//./src/tests/test1.txt
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        System.out.println("Enter code file location: ");
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
 
-        CharStream in = CharStreams.fromFileName("./src/tests/test1.txt");
+        // Reading data using readLine
+        String inputFile = reader.readLine();
+
+        CharStream in = CharStreams.fromFileName(inputFile);
         JavaGrLexer lexer = new JavaGrLexer(in);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         JavaGrParser parser = new JavaGrParser(tokens);
@@ -17,6 +26,12 @@ public class Main {
         String str = new JavaVisitor().visit(parser.prog());
         System.out.println(str);
 
+        try (PrintWriter out = new PrintWriter("output.c")) {
+            out.println(str);
+        }
+        try (PrintWriter out = new PrintWriter("output.txt")) {
+            out.println(str);
+        }
     }
 
 
